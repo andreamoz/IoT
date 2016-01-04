eede9d19e54e636c82ad935a962b970456fa2c28
GPA Andrea Maietta <amaietta@gpa.lan>
2016-01-04 17:43:15 +0100

    Aggiunta la prima versione del codice del primo prototipo

---
+++ cap02/listato_02_01/listato_02_01.ino
@@ -0,0 +1,12 @@
+int pinSensore = A0;
+
+void setup() {
+}
+
+void loop() {
+  int lettura = analogRead(pinSensore);
+  float tensione = lettura * 5000 / 1024;
+  float temperatura = (tensione - 500) / 10;
+
+  delay(1000);
+}
